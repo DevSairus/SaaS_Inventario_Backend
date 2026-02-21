92519bcf2fc9ad5d37fe32d2b89ee56efb4dde54
DevSairus <66100678+DevSairus@users.noreply.github.com>
2026-02-21 15:37:19 -0500

    error en Vercel

--- src/config/database.js
+++ src/config/database.js
@@ -9,7 +9,6 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-
 const { Sequelize } = require('sequelize');
 
 // ============================================================================
